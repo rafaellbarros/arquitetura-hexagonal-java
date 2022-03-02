1b70210e77b8333f6bb9248a1f8786b18b26cc05
Rafael <rafaelbarros.df@gmail.com>
2022-03-02 10:36:04 -0300

    :construction: Criando estrutura do projeto em springboot

---
+++ hexagonal-springboot/src/main/java/br/com/rafaellbarros/HexagonalSpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.rafaellbarros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonalSpringbootApplication.class, args);
+	}
+
+}
